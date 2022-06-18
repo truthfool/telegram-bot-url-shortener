6e415e910d5e5bc61ce2969772556b4c9b3b71c9
shani <ishanranasingh@gmail.com>
2022-06-18 11:42:19 +0530

    using sqllite db

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -78,21 +78,13 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "urldb",
-        "USER": "ishan",
-        "PASSWORD": "Chintu@98",
-        'HOST': 'localhost'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
