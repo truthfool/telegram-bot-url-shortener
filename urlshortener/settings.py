215222fa10ad657e4af2cdc017d267af68faf576
shani <ishanranasingh@gmail.com>
2022-06-29 12:19:00 +0530

    env variable path correction

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://url-shortener-telegram-bot.herokuapp.com/']
+ALLOWED_HOSTS = ['https://url-shortener-telegram-bot.herokuapp.com/','localhost', '127.0.0.1']
 
 
 # Application definition
