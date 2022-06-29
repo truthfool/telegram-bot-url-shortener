1a032c2f5ba6cfd10426429160961a67b53bab26
shani <ishanranasingh@gmail.com>
2022-06-29 11:28:02 +0530

    changed paths

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import load_dotenv,find_dotenv
 import os
 
-load_dotenv('../.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(find_dotenv())
+# load_dotenv('../.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
